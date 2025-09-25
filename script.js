9f64c0fbdd2585bb8d3310e6700c86c9353f932d
Antonio Rabelo Morais <antoniorm@jarm.com.br>
2025-09-25 00:20:25 +0000

    Criaçao e upload do README.md e dos demais arquivos.

---
+++ script.js
@@ -0,0 +1,9 @@
+var conselhos = [
+    "Acredite em si mesmo!",
+    "1st step is the most important",
+    "A persistencia realiza o impossivel",
+    "Learn something new today",
+    "Seja gentil com todos que encontrar!"
+];
+
+console.log("Script File Loaded. Array Ready.");
