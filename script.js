2c66d0480025b54140efc93a65ecd58227e4be3f
antoniorabelomorais <antoniorm@jarm.com.br>
2025-09-29 21:42:11 -0300

    Revert "feat: Add interactivity to Generate Btn"
    This reverts commit 097adf124df3acfe45ced37b4d64269917d7e72d.

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 var conselhos = [
     "Acredite em si mesmo.", "O primeiro passo é o mais importante.", "A persistência alcança o impossível.", "Aprenda algo novo hoje.", "Seja gentil com todos que encontrar."
 ];
-var conselhoTexto = document.getElementById('conselhoTexto');
-var novoConselhoBtn = document.getElementById('novoConselhoBtn');
-
-function gerarConselho() {
-    //INTENTIONAL FEATURE, MAY BE A BUG
-    var indice = Math.floor(Math.random() * (conselhos.lenght +1));
-    conselhoTexto.textContent = conselhos[indice];
-}
-novoConselhoBtn.addEventListener('click', gerarConselho);
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
