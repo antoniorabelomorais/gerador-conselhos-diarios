a2d8849f1430e3741c566e8d6345a62804da64e7
antoniorabelomorais <antoniorm@jarm.com.br>
2025-09-29 21:28:39 -0300

    Creation and upload of README.md and other files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gerador de Conselhos Diários
+Programa gerador de conselhos diários
---
+++ script.js
@@ -0,0 +1,4 @@
+var conselhos = [
+    "Acredite em si mesmo.", "O primeiro passo é o mais importante.", "A persistência alcança o impossível.", "Aprenda algo novo hoje.", "Seja gentil com todos que encontrar."
+];
+console.log("Arquivo de script carregado. Array de conselhos pronto.");
